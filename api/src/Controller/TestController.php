39da492c6134958589c6271acc183271438742c6
Tomek <tomasz.mackiewicz79@gmail.com>
2020-07-21 23:43:15 +0200

    Swagger

--- api/src/Controller/TestController.php
+++ api/src/Controller/TestController.php
@@ -5,9 +5,21 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Swagger\Annotations as SWG;
 
 class TestController extends AbstractFOSRestController
 {
+    /**
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns info if api works",
+     *     @SWG\Schema(
+     *         type="string",
+     *         example={"Api works!"}
+     *     )
+     * )
+     * @return string
+     */
     public function apiWorks()
     {
         $view = $this->view('Api works!', 200);
