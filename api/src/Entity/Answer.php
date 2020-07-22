8cf3853569030637f0ceee6bcdbab7fab349089e
Tomek <tomasz.mackiewicz79@gmail.com>
2020-07-22 18:57:54 +0200

    Fix path to repository

--- api/src/Entity/Answer.php
+++ api/src/Entity/Answer.php
@@ -7,7 +7,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=AnswerRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\AnswerRepository")
  * @ORM\Table(name="answers")
  */
 class Answer
