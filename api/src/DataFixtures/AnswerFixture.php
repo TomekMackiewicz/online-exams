b146864ea277eb58bdd4d333b34939d02424ad2a
Tomek <tomasz.mackiewicz79@gmail.com>
2020-07-24 20:44:25 +0200

    User class & fixtures groups

--- api/src/DataFixtures/AnswerFixture.php
+++ api/src/DataFixtures/AnswerFixture.php
@@ -1,12 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Answer;
 
-class AnswerFixture extends Fixture
+class AnswerFixture extends Fixture implements FixtureGroupInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -18,5 +21,10 @@ class AnswerFixture extends Fixture
         $manager->persist($answer);
 
         $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['answer'];
     }
 }
---
+++ api/src/DataFixtures/UserFixture.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use App\Entity\User;
+
+class UserFixture extends Fixture implements FixtureGroupInterface
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+         $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('test@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'test'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['user'];
+    }
+}
