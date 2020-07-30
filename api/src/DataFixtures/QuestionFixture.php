811b39dfe9b4b72ff3002ccf798598bb8f68c036
Tomek <tomasz.mackiewicz79@gmail.com>
2020-07-30 06:07:38 +0200

    Question controller test

---
+++ api/src/DataFixtures/QuestionFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Question;
+
+class QuestionFixture extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $question = new Question();
+        $question->setLabel('Were Ross and Rachel were on a break?');
+        $question->setDescription('Example description');
+        $question->setType('radio');
+        $question->setHint('Example hint');
+        $question->setIsRequired(true);
+        $manager->persist($question);
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['question'];
+    }
+}
---
+++ api/tests/Controller/QuestionControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use App\Tests\InitWebTestCase;
+use App\DataFixtures\QuestionFixture;
+
+class QuestionControllerTest extends InitWebTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testGetQuestion()
+    {
+        $this->addFixture(QuestionFixture::class);
+        $this->client->request('GET', '/api/v1/question/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testGetQuestions()
+    {
+        $this->addFixture(QuestionFixture::class);
+        $this->client->request('GET', '/api/v1/question');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testPostQuestion()
+    {
+        $this->client->request('POST', '/api/v1/question', [
+            'label' => 'Example label',
+            'description' => 'Example description',
+            'type' => 'radio',
+            'hint' => 'Example hint',
+            'is_required' => true
+        ]);
+
+        $this->assertEquals(201, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testPatchQuestion()
+    {
+        $this->addFixture(QuestionFixture::class);
+        $this->client->request('PATCH', '/api/v1/question/1', [
+            'label' => 'New label'
+        ]);
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testDeleteQuestion()
+    {
+        $this->addFixture(QuestionFixture::class);
+        $this->client->request('DELETE', '/api/v1/question/1');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+}
