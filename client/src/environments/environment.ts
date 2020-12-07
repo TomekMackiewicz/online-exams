4b252e3fd1a01a146567e09e315fe2691c102cf1
Tomek Mackiewicz <tomasz.mackiewicz79@gmail.com>
2020-12-07 17:29:58 +0100

    Docker fixes

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  base_url: 'http://localhost:8000/api/v1',
-  admin_url: 'http://localhost:8000/api/v1/admin',
-  user_url: 'http://localhost:8000/api/v1/user'
+  base_url: 'http://localhost:82/api/v1',
+  admin_url: 'http://localhost:82/api/v1/admin',
+  user_url: 'http://localhost:82/api/v1/user'
 };
